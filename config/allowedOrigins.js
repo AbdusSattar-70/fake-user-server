124f99b9e195c3fa1330172808ca983667470f54
Abdus Sattar <sasorganicagro@gmail.com>
2023-12-16 18:57:11 +0600

    Fix cors issue and Add localhost origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
-  'https://adminpanelclient.onrender.com',
   'http://127.0.0.1:3000',
   'http://localhost:3001',
+  'http://localhost:3000',
 ];
 
 module.exports = allowedOrigins;
