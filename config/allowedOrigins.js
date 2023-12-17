691dd7476ca5ce5d11f559f556170e71386d1f0d
Abdus Sattar <sasorganicagro@gmail.com>
2023-12-18 00:13:50 +0600

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   'http://localhost:5173',
-  'http://127.0.0.1:3000',
+  'https://fake-user-client.onrender.com',
   'http://localhost:3001',
   'http://localhost:3000',
 ];
