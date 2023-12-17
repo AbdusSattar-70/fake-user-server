4c0fcabbe9356ddacc78efc9af98af8ba348d065
Abdus Sattar <sasorganicagro@gmail.com>
2023-12-18 01:17:53 +0600

    Delete config directory

--- config/allowedOrigins.js
+++
@@ -1,8 +0,0 @@
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://fake-user-client.onrender.com',
-  'https://657f439cfcc4911ec1017341--cozy-sawine-036bc9.netlify.app/',
-  'http://localhost:3000',
-];
-
-module.exports = allowedOrigins;
