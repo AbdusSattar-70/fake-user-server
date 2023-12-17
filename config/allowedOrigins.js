4fb6ca7a4f3aa20a30c55ee45e221998579b272f
Abdus Sattar <sasorganicagro@gmail.com>
2023-12-18 00:55:28 +0600

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   'http://localhost:5173',
   'https://fake-user-client.onrender.com',
-  'http://localhost:3001',
+  'https://657f439cfcc4911ec1017341--cozy-sawine-036bc9.netlify.app/',
   'http://localhost:3000',
 ];
 
